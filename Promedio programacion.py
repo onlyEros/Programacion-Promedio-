c6c1dc9f30ef4cad1ddd8c6e6d2be87de71c6a8b
onlyEros <111711910+onlyEros@users.noreply.github.com>
2022-08-21 19:18:46 -0500

    Add files via upload

---
+++ Promedio programacion.py
@@ -0,0 +1,20 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Ingrese las 10 calificaciones")
+	c1 = float(input())
+	c2 = float(input())
+	c3 = float(input())
+	c4 = float(input())
+	c5 = float(input())
+	c6 = float(input())
+	c7 = float(input())
+	c8 = float(input())
+	c9 = float(input())
+	c10 = float(input())
+	p = (c1+c2+c3+c4+c5+c6+c7+c8+c9+c10)/10
+	print("Tu promedio es: ",p)
+
